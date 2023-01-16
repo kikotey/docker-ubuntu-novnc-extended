37e446c4663bac8fc93bd5d3d553afb77894b1ec
jack windy crosnier de bellaistre <81591152+bellaistre@users.noreply.github.com>
2023-01-16 14:00:04 +0100

    Update install-firefox-developer.sh

--- scripts/desktop-soft-install-script/firefox_developer_edition/install-firefox-developer.sh
+++ scripts/desktop-soft-install-script/firefox_developer_edition/install-firefox-developer.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sudo apt-get --purge -y autoremove firefox
+sudo rm -Rf /root/.mozilla/
+sudo rm /etc/firefox/
+sudo rm -Rf /etc/firefox/
+sudo rm -Rf /usr/lib/firefox
+sudo rm -Rf /usr/lib/firefox-addons/
+
 sudo install firefox-dev-edition-update /usr/local/sbin/
 sudo firefox-dev-edition-update -i
 cp -a firefoxDevEdition.desktop /usr/share/applications/
