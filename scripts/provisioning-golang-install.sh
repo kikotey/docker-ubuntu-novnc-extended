92c873cf3f324fb46da1900548ba8fe8b6c5c403
jack windy crosnier de bellaistre <81591152+bellaistre@users.noreply.github.com>
2023-01-16 13:12:34 +0100

    Update provisioning-golang-install.sh

--- scripts/provisioning-golang-install.sh
+++ scripts/provisioning-golang-install.sh
@@ -6,11 +6,14 @@ tar -xzf go1.19.3.linux-amd64.tar.gz -C .
 cp  go/bin/go /bin/go
 echo "export PATH=$PATH:~/go/bin" >> /etc/profile
 echo "export GOPATH=/go" >> ~/.bashrc
-echo "export GOROOT=/bin/go" >> ~/.bashrc
+echo "export GOROOT=/home/scripts/go" >> ~/.bashrc
 echo "export GOPATH=/go" >> /root/.bashrc
-echo "export GOROOT=/bin/go" >> /root/.bashrc
+echo "export GOROOT=/home/scripts/go" >> /root/.bashrc
 source /etc/profile
 source ~/.bashrc
 source /root/.bashrc
+
+export GOPATH=/go
+export GOROOT=/home/scripts/go
 
 go version
