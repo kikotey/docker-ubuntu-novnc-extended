44cfd964253b3ebf00656d78f666af127f21308b
jack windy crosnier de bellaistre <81591152+bellaistre@users.noreply.github.com>
2023-01-19 16:28:15 +0000

    CDBJ - Update nvm install

--- scripts/provisioning-nvm-install.sh
+++ scripts/provisioning-nvm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-cat nvm_bashrc >> ~/.bash_profile
-cat nvm_bashtc >> ~/.zshrc
+cat nvm-bashrc >> ~/.bash_profile
+cat nvm-bashrc >> ~/.zshrc
 source ~/.bashrc
 #nvm ls-remote
 nvm list
